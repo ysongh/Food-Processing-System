742a957008a10a66a7f7454414c45d1430f4c70a
You Song Hou <yousonghou@yahoo.com>
2020-09-15 23:06:53 -0400

    add a main page for task section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Container } from '@material-ui/core';
 import BottomNav from './component/layout/BottomNav';
 
 import Home from './component/Home';
+import MainTask from './component/tasks/MainTask';
 import Tasks from './component/tasks/Tasks';
 import AddWarehouse from './component/AddWarehouse';
 import QRcode from './component/QRcode';
@@ -15,7 +16,8 @@ function App() {
     <Router className="App">
       <Container className="main" maxWidth="sm">
         <Route exact path="/" component={Home} />
-        <Route exact path="/tasks" component={Tasks} />
+        <Route exact path="/task/main" component={MainTask} />
+        <Route exact path="/task/tasks" component={Tasks} />
         <Route exact path="/addwarehouses" component={AddWarehouse} />
         <Route exact path="/qrreader" component={QRcode} />
         <Route exact path="/createqr" component={CreateQR} />
---
+++ src/component/tasks/MainTask.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles({
+    container: {
+        marginTop: '5rem'
+    },
+    btn: {
+        display: 'block',
+        width: '14rem',
+        margin: '2rem auto',
+        fontSize: '2rem',
+        textAlign: 'center'
+    }
+});
+
+const MainTask = () => {
+    const classes = useStyles();
+
+    return(
+        <div className={classes.container}>
+            <Button className={classes.btn} component={Link} to="/task/main" variant="contained">
+                New Task
+            </Button>
+            <br />
+            <Button className={classes.btn} component={Link} to="/task/tasks" variant="contained">
+                Ongoing Task
+            </Button>
+            <br />
+            <Button className={classes.btn} component={Link} to="/task/main" variant="contained">
+                Completed Task
+            </Button>
+        </div>
+    )
+}
+
+export default MainTask;
