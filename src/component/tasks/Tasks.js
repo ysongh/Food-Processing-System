306d4b4b16d2a92070deffef145a1cdf9694ba6c
You Song Hou <yousonghou@yahoo.com>
2020-09-15 22:26:37 -0400

    add search bars for the task page

--- src/component/tasks/Tasks.js
+++ src/component/tasks/Tasks.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import TextInputField from '../common/TextInputField';
+
 const Tasks = () => {
+    const [gtin, setgtin] = useState("");
+    const [gln, setgln] = useState("");
     const [warehouses] = useState([
         {
             "name": "Warehouse #1",
@@ -23,6 +27,20 @@ const Tasks = () => {
             <Link to="/addwarehouses">
                 Add Warehouses
             </Link>
+            <TextInputField
+                label="Please Input GTIN"
+                name="gtin"
+                type="number"
+                value={gtin}
+                onChange={(e) => setgtin(e.target.value)}
+            />
+            <TextInputField
+                label="Please Input GLN"
+                name="gln"
+                type="number"
+                value={gln}
+                onChange={(e) => setgln(e.target.value)}
+            />
             { warehouses.map((warehouse, index)  => {
                 return (
                     <div key={index}>
