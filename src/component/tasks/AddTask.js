7e27c147e338a22655d64011449b3e10de690111
You Song Hou <yousonghou@yahoo.com>
2020-09-16 22:44:59 -0400

    add a function to call create task endpoint

--- src/component/tasks/AddTask.js
+++ src/component/tasks/AddTask.js
@@ -1,13 +1,29 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 
+import axios from '../../axios';
 import TextInputField from '../common/TextInputField';
 import TextAreaField from '../common/TextAreaField';
 
-const AddWarehouse = () => {
+const AddTask = () => {
+    const history = useHistory();
+
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
+    const [detail, setDetail] = useState("");
+    const [destination, setDestination] = useState("");
+
+    const onSubmit = async () => {
+        try{
+            const taskData = { title, description, detail, destination }
+            await axios.post('/task/create', taskData);
+
+            history.push('/task/main');
+        } catch(err){
+            console.error(err);
+        }
+    }
 
     return(
         <>
@@ -26,12 +42,26 @@ const AddWarehouse = () => {
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
             />
+            <TextAreaField
+                label="Detail of this Task"
+                name="detail"
+                type="text"
+                value={detail}
+                onChange={(e) => setDetail(e.target.value)}
+            />
+            <TextInputField
+                label="Destination"
+                name="destination"
+                type="text"
+                value={destination}
+                onChange={(e) => setDestination(e.target.value)}
+            />
 
-            <Button component={Link} to="/task/main" variant="contained">
-                Next
+            <Button onClick={() => onSubmit()} variant="contained">
+                Create
             </Button>
         </>
     )
 }
 
-export default AddWarehouse;
+export default AddTask;
