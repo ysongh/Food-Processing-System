398559fa65fd88e3d882edfe85f67df8eb3e9783
You Song Hou <yousonghou@yahoo.com>
2020-09-19 00:57:02 -0400

    show different buttons for certain users on the main task page

--- src/component/tasks/MainTask.js
+++ src/component/tasks/MainTask.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+
+import { GlobalContext } from '../../context/GlobalState';
 
 const useStyles = makeStyles({
     container: {
@@ -19,13 +21,18 @@ const useStyles = makeStyles({
 });
 
 const MainTask = () => {
+    const { user } = useContext(GlobalContext);
     const classes = useStyles();
 
     return(
         <div className={classes.container}>
-            <Button className={classes.btn} component={Link} to="/task/add" variant="contained">
-                New Task
-            </Button>
+            { user.type === 'Owner' ? (<Button className={classes.btn} component={Link} to="/task/add" variant="contained">
+                Create Task
+                </Button>) : (
+                <Button className={classes.btn} component={Link} to="/task/tasks/false" variant="contained">
+                    New Task
+                </Button>
+            ) }
             <br />
             <Button className={classes.btn} component={Link} to="/task/tasks/false" variant="contained">
                 Ongoing Task
